560d554f4c442f538306b35ac55149f398cf139b
Zachariah Crowell <zachariah.crowell@techtonic.com>
2019-09-24 12:04:08 -0600

    Router working to display home and playlist routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,42 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 export class NavBar extends Component {
-    state = {
-        navOpen: false,
-    }
+  state = {
+    navOpen: false
+  };
   openNav = () => {
     document.getElementById("mySidenav").style.width = "250px";
-    this.setState({ navOpen: true})
-  }
+    this.setState({ navOpen: true });
+  };
 
   closeNav = () => {
     document.getElementById("mySidenav").style.width = "0";
-    this.setState({ navOpen: false})
-  }
+    this.setState({ navOpen: false });
+  };
 
   render() {
-      const { navOpen } = this.state
-      const { displayName, userImage } = this.props
+    const { navOpen } = this.state;
+    const { displayName, userImage } = this.props;
     return (
       <div>
-        { !navOpen &&
-        <span
-          className="menu-toggle"
-          onClick={this.openNav}
-        >
-          &#9776;
-        </span>
-        }
+        {!navOpen && (
+          <span className="menu-toggle" onClick={this.openNav}>
+            &#9776;
+          </span>
+        )}
         <div id="mySidenav" className="sidenav">
           <p className="closebtn" onClick={this.closeNav}>
             &times;
           </p>
           <img className="avatar" alt="profile avatar" src={userImage} />
           <p className="username">{displayName}</p>
-          <a href="/">About</a>
-          <a href="/">Services</a>
-          <a href="/">Clients</a>
-          <a href="/">Contact</a>
+          <Link to="/playlists">Playlists</Link>
+          <Link to="/">Nav 2</Link>
+          <Link to="/">Nav 3</Link>
+          <Link to="/">Nav 4</Link>
+          <Link to="/">Home</Link>
         </div>
       </div>
     );
